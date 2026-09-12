b233d6d30c6b7966392cfa716b7f4004c28a245a
Sivani <sivanibommaraju@gmail.com>
2026-09-12 20:57:38 +0530

    Allow deployed frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,8 +19,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-    ],
+    "http://localhost:5173",
+    "https://crop-yield-prediction-sivani-frontend.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
